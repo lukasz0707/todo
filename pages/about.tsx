03fe2274e333f398634f01733cea8bff743a2043
lukasz0707 <lukaszs0707@gmail.com>
2023-02-14 03:41:49 +0100

    .

--- pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function about() {
-  return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
-      <h1>about to be implemented :)</h1>
-    </div>
-  )
-}
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>Hello</h1>
-    </div>
-  )
-}
