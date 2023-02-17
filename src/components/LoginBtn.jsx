fea90547b9a46d73974df0770e934a0f5ff68620
lukasz0707 <lukaszs0707@gmail.com>
2023-02-17 01:44:36 +0100

    login nav

--- src/components/LoginBtn.jsx
+++ src/components/LoginBtn.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 export default function LoginBtn(props) {
   return (
     <Link to={"/login"}>
-      <button className="bg-cyan-400  hover:bg-cyan-600 text-white font-bold py-2 px-6 rounded-full hidden lg:inline-block">
+      <li className="mx-auto mt-10 block rounded-full bg-cyan-400 py-2 px-6 text-center font-bold text-white hover:bg-cyan-600 lg:inline-block">
         {/* {props.accessToken ? "Logout" : "Login"} */}Login
-      </button>
+      </li>
     </Link>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,45 +2,46 @@ import React, { useState } from "react"
 import { RxHamburgerMenu } from "react-icons/rx"
 import { AiOutlineClose } from "react-icons/ai"
 import LoginBtn from "./LoginBtn"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false)
   return (
-    <nav className={`w-full lg:bg-none select-none ${navbar ? "fixed inset-0 bg-black" : "block"}`}>
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl lg:items-baseline lg:flex lg:px-8">
+    <nav className={`w-full select-none lg:bg-none ${navbar ? "fixed inset-0 bg-black" : "block"}`}>
+      <div className="mx-auto justify-between px-4 lg:flex lg:max-w-7xl lg:items-baseline lg:px-8">
         <div>
-          <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
+          <div className="flex items-center justify-between py-3 lg:block lg:py-5">
             {/* LOGO */}
-            <a href="/" onClick={() => setNavbar(false)}>
-              <h2 className="text-4xl text-white font-bold ">TodoApp</h2>
-            </a>
+            <Link to="/" onClick={() => setNavbar(false)}>
+              <h2 className="text-4xl font-bold text-white ">TodoApp</h2>
+            </Link>
             {/* HAMBURGER BUTTON FOR MOBILE */}
             <div className="lg:hidden">
-              <button className="p-2 text-white rounded-lg outline-none" onClick={() => setNavbar(!navbar)}>
+              <button className="rounded-lg p-2 text-white outline-none" onClick={() => setNavbar(!navbar)}>
                 {navbar ? <AiOutlineClose size={35} /> : <RxHamburgerMenu size={35} />}
               </button>
             </div>
           </div>
         </div>
         <div>
-          <div className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${navbar ? "p-12 lg:p-0 block" : "hidden"}`}>
-            <ul className="h-screen lg:h-auto place-items-baseline justify-center lg:flex text-2xl">
-              <a href="/todos" onClick={() => setNavbar(!navbar)}>
-                <li className="pb-6  text-white py-2 lg:px-6 text-center border-b-2 lg:border-b-0  hover:bg-slate-900 border-cyan-800   lg:hover:text-cyan-500 lg:hover:bg-transparent">
+          <div className={`mt-8 flex-1 justify-self-center pb-3 lg:mt-0 lg:block lg:pb-0 ${navbar ? "block p-12 lg:p-0" : "hidden"}`}>
+            <ul className="h-screen place-items-baseline justify-center text-2xl lg:flex lg:h-auto">
+              <Link to="/todos" onClick={() => setNavbar(!navbar)}>
+                <li className="border-b-2  border-cyan-800 py-2 pb-6 text-center text-white hover:bg-slate-900  lg:border-b-0 lg:px-6   lg:hover:bg-transparent lg:hover:text-cyan-500">
                   Todos
                 </li>
-              </a>
-              <a href="/friends" onClick={() => setNavbar(!navbar)}>
-                <li className="pb-6 text-white py-2 px-6 text-center  border-b-2 lg:border-b-0  hover:bg-slate-900 border-cyan-800  lg:hover:text-cyan-500 lg:hover:bg-transparent">
+              </Link>
+              <Link to="/friends" onClick={() => setNavbar(!navbar)}>
+                <li className="border-b-2 border-cyan-800 py-2 px-6 pb-6  text-center text-white  hover:bg-slate-900 lg:border-b-0  lg:hover:bg-transparent lg:hover:text-cyan-500">
                   Friends
                 </li>
-              </a>
-              <a href="/settings" onClick={() => setNavbar(!navbar)}>
-                <li className="pb-6 text-white py-2 px-6 text-center  border-b-2 lg:border-b-0  hover:bg-slate-900  border-cyan-800  lg:hover:text-cyan-500 lg:hover:bg-transparent">
+              </Link>
+              <Link to="/settings" onClick={() => setNavbar(!navbar)}>
+                <li className="border-b-2 border-cyan-800 py-2 px-6 pb-6  text-center text-white  hover:bg-slate-900  lg:border-b-0  lg:hover:bg-transparent lg:hover:text-cyan-500">
                   Settings
                 </li>
-              </a>
-                <LoginBtn/>
+              </Link>
+              <LoginBtn />
             </ul>
           </div>
         </div>
